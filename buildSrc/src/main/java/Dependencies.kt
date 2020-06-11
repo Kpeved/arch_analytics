e467c1ceb7a1b0d792dad827916006ada66e4975
kpeved <uakpevedko@gmail.com>
2020-06-11 01:07:35 +0100

    Add Const into Dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -4,64 +4,64 @@ Find updates by running:
  */
 
 object Modules {
-    val core = ":core"
-    val coreUi = ":core_ui"
-    val coreNetwork = ":core_network"
-    val coreAnalytics = ":core_analytics"
-    val coreAnalyticsConstants = ":core_analytics_constants"
+    const val core = ":core"
+    const val coreUi = ":core_ui"
+    const val coreNetwork = ":core_network"
+    const val coreAnalytics = ":core_analytics"
+    const val coreAnalyticsConstants = ":core_analytics_constants"
 
-    val featureDashboard = ":feature_dashboard"
-    val featureDashboardAnalytics = ":feature_dashboard_analytics"
+    const val featureDashboard = ":feature_dashboard"
+    const val featureDashboardAnalytics = ":feature_dashboard_analytics"
 
-    val featureHome = ":feature_home"
-    val featureHomeAnalytics = ":feature_home_analytics"
+    const val featureHome = ":feature_home"
+    const val featureHomeAnalytics = ":feature_home_analytics"
 
-    val featureNotifications = ":feature_notifications"
-    val featureNotificationsAnalytics = ":feature_notifications_analytics"
+    const val featureNotifications = ":feature_notifications"
+    const val featureNotificationsAnalytics = ":feature_notifications_analytics"
 }
 
 object Versions {
-    val kotlin = "1.3.70"
-    val compileSdk = 29
-    val targetSdk = 29
-    val minSdk = 21
-    val buildTools = "29.0.3"
+    const val kotlin = "1.3.70"
+    const val compileSdk = 29
+    const val targetSdk = 29
+    const val minSdk = 21
+    const val buildTools = "29.0.3"
 
-    val androidGradlePlugin = "3.5.3"
-    val gradleVersionPlugin = "0.27.0"
+    const val androidGradlePlugin = "3.5.3"
+    const val gradleVersionPlugin = "0.27.0"
 
-    val coreKtx = "1.2.0-rc01"
-    val fragmentKtx = "1.2.0-rc01"
-    val androidXConstraintLayout = "2.0.0-beta4"
-    val appCompat = "1.2.0-alpha01"
-    val recyclerView = "1.1.0"
-    val swipeRefreshLayout = "1.1.0-beta01"
+    const val coreKtx = "1.2.0-rc01"
+    const val fragmentKtx = "1.2.0-rc01"
+    const val androidXConstraintLayout = "2.0.0-beta4"
+    const val appCompat = "1.2.0-alpha01"
+    const val recyclerView = "1.1.0"
+    const val swipeRefreshLayout = "1.1.0-beta01"
 
-    val navigation = "2.3.0-alpha03"
-    val material = "1.2.0-alpha05"
-    val retrofit = "2.7.1"
-    val okhttpLogging = "4.3.0"
+    const val navigation = "2.3.0-alpha03"
+    const val material = "1.2.0-alpha05"
+    const val retrofit = "2.7.1"
+    const val okhttpLogging = "4.3.0"
 
-    val koin = "2.1.3"
-    val timber = "4.7.1"
-    val lottie = "3.3.1"
+    const val koin = "2.1.3"
+    const val timber = "4.7.1"
+    const val lottie = "3.3.1"
 
-    val viewModel = "2.2.0"
+    const val viewModel = "2.2.0"
 
-    val coroutines = "0.22.5"
-    val coroutinesReactive = "1.3.3"
+    const val coroutines = "0.22.5"
+    const val coroutinesReactive = "1.3.3"
 
-    val paris = "1.4.0"
+    const val paris = "1.4.0"
 
-    val mpAndroidChart = "v3.1.0"
+    const val mpAndroidChart = "v3.1.0"
 
-    val junit = "4.13"
-    val junitExt = "1.1.1"
-    val espresso = "3.2.0"
+    const val junit = "4.13"
+    const val junitExt = "1.1.1"
+    const val espresso = "3.2.0"
 
-    val firebaseAnalytics = "17.2.3"
-    val firebaseCrashlytics = "17.0.0-beta02"
-    val firebaseMessaging = "20.1.3"
+    const val firebaseAnalytics = "17.2.3"
+    const val firebaseCrashlytics = "17.0.0-beta02"
+    const val firebaseMessaging = "20.1.3"
 }
 
 object Deps {
